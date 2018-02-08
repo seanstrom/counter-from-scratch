eca49645c6f8ddecec3ad8e69013246f223ae275
seantrom <sean@seanhagstrom.com>
2018-02-07 17:16:51 -0800

    change app to render inside of the body tag

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import { main } from './main'
 import './index.scss'
 
 
-const mountNodeId = 'app'
-const mountNode = document.getElementById(mountNodeId)
-
-
-main({ window: window, node: mountNode })
+main({
+  window,
+  node: document.body
+})
